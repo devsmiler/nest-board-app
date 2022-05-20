f430832a0cf26998a80e70b101d9b808b54516df
Min Kyu Kim <devsmile@twc.co.kr>
2022-05-20 18:11:05 +0900

    init comments module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule, CommentsModule],
   controllers: [],
   providers: [],
 })
