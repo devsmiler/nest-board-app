01399d396700033933124b88b7a4955478380689
Min Kyu Kim <devsmile@twc.co.kr>
2022-05-25 01:10:03 +0900

    add comment entity

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,18 @@
-export class Comment {}
+import { User } from 'src/auth/user.entity';
+import { Board } from 'src/boards/ board.entity';
+import { Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // 한명의 유저는 여러개의 코멘트를 가질수 있다.
+  @ManyToOne(() => User, (user) => user.id)
+  user: number;
+
+  @ManyToOne(() => Board, (board) => board.id)
+  article: number;
+
+  @Column({ nullable: false })
+  contents: string;
+}
